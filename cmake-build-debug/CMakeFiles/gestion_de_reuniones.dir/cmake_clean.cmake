29f1369c98ac63701700ae45630d644f841254cd
Bryamslm <bryamlopez1882002@estudiantec.cr>
2021-03-27 16:05:10 -0600

    Se carga el proyecto vacio

---
+++ cmake-build-debug/CMakeFiles/gestion_de_reuniones.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gestion_de_reuniones.dir/main.cpp.obj"
+  "gestion_de_reuniones.exe"
+  "gestion_de_reuniones.exe.manifest"
+  "gestion_de_reuniones.pdb"
+  "libgestion_de_reuniones.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gestion_de_reuniones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * Gestion de Reuniones
+ *      Luz Mora
+ *      Bryam Lopez
+ *      Estefania Hidalgo
+ */
+
+int main() {
+    cout << "Hello, World!" <<endl;
+    return 0;
+}
