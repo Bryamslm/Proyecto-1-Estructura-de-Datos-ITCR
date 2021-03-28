8f1dd478dac7e2a0ede4e4e5ccd93c8e7d357360
Bryamslm <bryamlopez1882002@estudiantec.cr>
2021-03-28 13:05:28 -0600

    Se creó la lista de admin con opcion de agregar admin

--- main.cpp
+++ main.cpp
@@ -2,20 +2,112 @@
 using namespace std;
 
 /*
- * Gestion de Reuniones
+ * Meeting management
  *      Luz Mora
  *      Bryam Lopez
  *      Estefania Perez Hidalgo
  */
 
-int main() {
-    int n;
 
-    cout << "Hello, World!" <<endl;
+//------ prototype section -----
 
-    cout<<"Hola mundo 2.0"<<endl;
-    cout<<"No me funciona"<<endl;
-    cout<<"Mentira, si me funciona"<<endl;
-    cout<<"Mesdsdsfsdfsdfciona"<<endl;
+void burnedData();
+bool addAdmin(string, int, string);
+
+//------ end prototype section -----
+
+struct Teachers{
+    string name;
+}*firstTeacher;
+
+struct Students{
+    string name;
+}*firstStudent;
+
+struct Courses{
+
+    string code;
+}*firstCourse;
+
+struct Administrators{
+
+    string fullName;
+    int id;
+    string gender;
+
+    Administrators*sig;
+    Teachers*subListT;
+    Students*subListS;
+    Courses*subListC;
+
+    Administrators(string fn, int i, string g){
+
+        fullName=fn;
+        id=i;
+        gender=g;
+        sig=NULL;
+        subListT=firstTeacher;
+        subListS=firstStudent;  //la sublista es igual a la lista general ya que es compartida
+        subListC=firstCourse;
+
+    }
+}*firstAdmin;
+
+Administrators*searchAdmin(string fullName, int id){//busca admin a ver si ya existe, retorna NULL si no está
+
+    Administrators*temp=firstAdmin;
+
+    while(temp != NULL){
+        if(temp->fullName==fullName){//Lo busca por nombre
+            cout<<"\nThe name is already part of the administrators\n"<<endl;
+            return temp;
+        }else if(temp->id==id){//Lo busca por ID
+            cout<<"\nThe ID is already part of the administrators\n"<<endl;
+            return temp;
+        }
+        temp=temp->sig;
+    }
+    return NULL; //if return NULL the admin is not
+
+}
+
+bool addAdmin(string fullName, int id, string gender){//función que agrega administadores a lista simple con inserción al inicio
+
+    if(firstAdmin==NULL){//si lista de admins está vacía se agrega nuevo admin
+        Administrators*nn = new Administrators(fullName, id, gender);
+        firstAdmin=nn;
+        return true;
+    }else{
+        Administrators*admin=searchAdmin(fullName, id);//si retorna  NULL el admin no existe, sino ya existe
+
+        if(admin != NULL){//est esta el admin
+            cout<<"Administrator could not be added"<<endl;
+
+            return false;
+        }else{
+            Administrators*nn= new Administrators(fullName, id, gender);//crea al admin
+
+            nn->sig=firstAdmin;
+            firstAdmin=nn;      //hace inserción al inicio de lista simple
+
+            return true;
+        }
+    }
+}
+
+void burnedData(){
+
+    addAdmin("Veronica Brenes", 1, "Female");
+    addAdmin("Christian Gonzales", 2, "Male");
+    addAdmin("Monica Speer", 3, "Female");
+    addAdmin("Julio Cesar", 4, "Male");
+    addAdmin("Sonia Rojas", 5, "Female");
+
+}
+
+int main(){
+
+    burnedData();
+
     return 0;
 }
